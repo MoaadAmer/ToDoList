48f013ab00ce715811e2b7d54f18aaa4de7b0006
Moaad Amer <moaad02@gmail.com>
2025-04-12 20:45:41 +0300

    1- prettify the page 2- add empty validation and existing item 3- suport delete item

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
-var input = document.getElementById("userInput");
-var button = document.getElementById("enter");
-var ul = document.querySelector("ul");
 
-function addEventListenerTolis() {
-    lis = ul.querySelectorAll("li");
-    lis.forEach(li => {
-        li.addEventListener("click", function () {
-            li.classList.toggle("done");
-        });
+function addEventListenerToShoppingItems() {
 
-    })
-}
-
-function addEventListenerToDelButton() {
-    lis = ul.querySelectorAll("li");
-    lis.forEach(li => {
-        li.querySelector("button").addEventListener("click", function () {
-            li.remove();
-        });
-
-    })
-}
-
-function inputLength() {
-    return input.value.length;
-}
-
-function createListElem() {
-    var li = document.createElement("li");
-    var btn = document.createElement("button");
-    btn.innerText = "Delete";
-    li.appendChild(document.createTextNode(input.value));
-    li.appendChild(btn);
-    ul.appendChild(li);
-    input.value = "";
-
-    addEventListenerTolis();
-    addEventListenerToDelButton();
-
-}
-
-function addListAfterClick() {
-    if (inputLength() > 0) {
-        createListElem();
+    let ShoppingItems = document.querySelectorAll("span");
+    for (let index = 0; index < ShoppingItems.length; index++) {
+        ShoppingItems[index].addEventListener('click', checkOrUncheckItem)
     }
 }
 
-function addListAfterKeyPress(event) {
-    if (inputLength() > 0 && event.keyCode === 13) {
-        createListElem();
-    }
+
+function addEventListenerToDeleteButtons() {
+
+    let deleteButtons = document.querySelectorAll('button.delete');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', deleteItem);
+    });
 }
 
-input.addEventListener("keydown", addListAfterKeyPress);
-button.addEventListener("click", addListAfterClick);
+
+function addEventListenerToInputButton() {
+
+    let input = document.querySelector('#item');
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            addItemToListProcess();
+        }
+    });
+}
+
+function addEventlistenerToAddButton() {
+    let addBtn = document.querySelector('#addBtn');
+    addBtn.addEventListener('click', addItemToListProcess);
+}
+
+function addItemToListProcess() {
+    let input = document.querySelector('#item');
+    if (isInputValid(input.value)) {
+        addItemTolist(input.value);
+        input.value = '';
+    } else {
+        alert('input is not valid')
+    }
+
+}
+
+function addItemTolist(item) {
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    span.appendChild(document.createTextNode(item));
+    span.addEventListener('click', checkOrUncheckItem);
+    li.appendChild(span);
+
+    let button = document.createElement('button')
+    button.appendChild(document.createTextNode('Delete'));
+    button.className = 'delete';
+
+    button.addEventListener('click', deleteItem);
+
+    li.appendChild(button);
+
+    let ol = document.querySelector('ol');
+    ol.appendChild(li);
+
+}
+
+function checkOrUncheckItem(event) {
+    event.currentTarget.classList.toggle('done');
+}
+
+function isInputValid(value) {
+    let shoppingItems = document.querySelectorAll("span");
+    if (value.length === 0) {
+        return false;
+    }
+    for (let index = 0; index < shoppingItems.length; index++) {
+        if (shoppingItems[index].textContent === value) {
+            return false;
+        }
+
+    }
+    return true;
+};
 
 
+function deleteItem(event) {
+    event.currentTarget.parentElement.remove();
+}
 
-addEventListenerTolis();
 
-addEventListenerToDelButton();
+addEventListenerToShoppingItems();
+addEventListenerToDeleteButtons();
+addEventListenerToInputButton();
+addEventlistenerToAddButton();
